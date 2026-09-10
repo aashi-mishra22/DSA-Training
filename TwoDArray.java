720ff4ae42a903df8c7ec398194004f40f3875d4
Aashi Mishra <mishraaashi073@gmail.com>
2026-09-10 15:55:45 +0530

    Add files via upload

---
+++ TwoDArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class TwoDArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows and cols: ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] arr = new int[r][c];
+
+        System.out.println("Enter " + r*c + " elements:");
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Your Matrix:");
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
